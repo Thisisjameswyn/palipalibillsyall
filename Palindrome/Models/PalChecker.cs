0cb338ceeca2bcc4d4d6b7fa7a081e66edfe1a7d
James Wyn <thisisjameswyn@gmail.com>
2021-05-04 14:36:46 -0700

    initial file setup
    co-authored-by: Marney Mallory <marney.mallory@gmail.com>

---
+++ Palindrome/Models/PalChecker.cs
@@ -0,0 +1,23 @@
+namespace PalChecker
+{
+  public class PalChecker
+  {
+    public string myString;
+    public PalChecker(string passedString)
+    {
+      myString = passedString;
+    }
+  }
+}
+
+
+
+
+
+//take an input from user (string)
+
+//take string and make a copy and reverse copy string.Reverse()
+
+//evaluate input against reversed self fro palindrome
+
+//return true if palindrome
